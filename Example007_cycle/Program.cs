8a8bbb11b0168cfcb84cd2dc1ebba7811e99b8bc
Katerina Grishaeva <ithis@yandex.ru>
2022-07-24 01:23:29 +0300

    Example007_cycle comments

--- Example007_cycle/Program.cs
+++ Example007_cycle/Program.cs
@@ -1,8 +1,6 @@
-﻿Console.Clear();
-//Console.SetCursorPosition(10, 4);
-//Console.WriteLine("+");
+﻿Console.Clear(); //очищаем окно терминала
 
-int xa = 40, ya = 1,
+int xa = 40, ya = 1, //задаем вершины треугольника и отрисовываем его символом +
     xb = 1, yb = 30,
     xc = 80, yc = 30;
 
@@ -16,16 +14,16 @@ Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
 
-int x = xa, y = ya;
+int x = xa, y = ya; //пусть первая случайная вершина равна вершине а
 int count = 0;
 
-while(count < 1000)
+while(count < 10000) //тестами определили количество циклов
 {
     int what = new Random().Next(0, 3); // [0;3) 0 1 2 генер случ число для опред случ вершины
     if (what == 0)
     {
-        x = (x + xa)/2;
-        y = (y + ya)/2;
+        x = (x + xa)/2; //исходя из координат случ вершины + точка с коорд (х, у)
+        y = (y + ya)/2; //вычисляем координаты середины отрезка
     }
     else
     {
@@ -41,7 +39,7 @@ while(count < 1000)
         }
     }
 
-    Console.SetCursorPosition(x, y);
+    Console.SetCursorPosition(x, y); //ставим + в коорд середины отрезка этой итерации
     Console.WriteLine("+");
     count = count + 1;
 }
