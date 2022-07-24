34d687c2cf0263a08e504b56e89c9b614ddabf3d
Katerina Grishaeva <ithis@yandex.ru>
2022-07-24 13:58:26 +0300

    Task005_progressive: input int number; output arithmetic progression [-N .. N]

---
+++ Task005_progressive/Program.cs
@@ -0,0 +1,34 @@
+﻿// Task005_progressive: input int number; output arithmetic progression [-N .. N]
+Console.WriteLine("Введите число ");
+bool result = int.TryParse(Console.ReadLine(), out int N);
+if (result != true) //проверка типа введенного значения
+{
+    Console.WriteLine("Вы ввели не целое число. Запустите программу снова.");
+    // оператор прерывания
+}
+else
+{
+
+    if(N < 0)
+    {
+        N = -N;
+    }
+
+
+    System.Console.WriteLine("Целые числа от -N до N");
+    System.Console.Write(-N); 
+    //избавляемся от запятой после последнего числа
+    //на нов строке печатаем первое число диапазона ДО цикла
+    //без перевода строки
+    //счетчик начинается со второго числа диапазона
+    int count = -N + 1;
+
+    while(count < (N + 1)) 
+    //если заданное число 0, то цикл не отработает
+    //запятая после напечатанного нуля не появится
+    {
+        System.Console.Write(", "); //запятую и пробел после предыдущего числа
+        System.Console.Write(count); //само число
+        count = count + 1;
+    }
+}
