c58b9c8e0c04f727f8a5e34d810ae0a4529c6354
Katerina Grishaeva <ithis@yandex.ru>
2022-07-31 20:56:54 +0300

    Example_200: find max of 9 numbers without methods

---
+++ 02_lesson/Example_200/Program.cs
@@ -0,0 +1,23 @@
+﻿// Example_200: find max of 9 numbers without methods
+int a1 = 43;
+int a2 = 33;
+int a3 = 63;
+int a4 = 23;
+int a5 = -657;
+int a6 = -4343;
+int a7 = 96;
+int a8 = 1;
+int a9 = 2;
+
+int max = a1;
+if (a2 > max) max = a2;
+if (a3 > max) max = a3;
+if (a4 > max) max = a4;
+if (a5 > max) max = a5;
+if (a6 > max) max = a6;
+if (a7 > max) max = a7;
+if (a8 > max) max = a8;
+if (a9 > max) max = a9;
+
+System.Console.Write("max = ");
+System.Console.WriteLine(max);
