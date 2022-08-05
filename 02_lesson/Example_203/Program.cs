f04b936d74aaa27b200c320fb58760d9331fa38e
Katerina Grishaeva <ithis@yandex.ru>
2022-08-05 16:48:56 +0300

    массив и поиск заданного элемента

---
+++ 02_lesson/Example_203/Program.cs
@@ -0,0 +1,41 @@
+﻿// Example_203: request x from the user; find index of elements = x
+
+System.Console.WriteLine("Введите количество элементов массива");
+System.Console.WriteLine("(натуральное число)");
+System.Console.WriteLine("N = ");
+
+bool result = int.TryParse(Console.ReadLine(), out int N);
+if ((result != true) | (N < 1)) //проверка типа введенного значения
+{
+    Console.WriteLine("Вы ввели не натуральное число. Запустите программу снова.");
+}    
+else
+    
+System.Console.WriteLine("Введите значения каждого элемента массива");
+int index = 0;
+int[] array = new int[N];;
+while(index < N)
+{
+    System.Console.Write("array[");
+    System.Console.Write(index);
+    System.Console.Write("] = ");
+    array[index] = System.Console.ReadLine();
+    index++;
+}    
+
+System.Console.WriteLine("Введите искомое значение среди элементов массива");
+System.Console.WriteLine("x =");
+int x = System.Console.ReadLine();
+
+while(index < N)
+{    
+    if(array[index] == x)
+    {
+        System.Console.Write("array[");
+        System.Console.Write(index);
+        System.Console.Write("] = ");
+        System.Console.WriteLine(x);
+    }
+    
+    index ++;
+}
---
+++ 02_lesson/Example_203_class/Program.cs
@@ -0,0 +1,17 @@
+﻿// Example_203_class по алгоритму на лекции
+
+int[] array = {80, 1, 52, 853, 4, 853, 36, 7785, 8};
+
+int n = array.Length;
+int find = 853;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
