c010289a0647824805f18188201d159ad9d404c0
Katerina Grishaeva <ithis@yandex.ru>
2022-08-07 13:55:36 +0300

    добавлены задачи с семинара 2

---
+++ 02_lesson/01Task/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+//Если число 2 не кратно числу 1, то программа выводит остаток от деления. 
+//34, 5 -> не кратно, остаток 4 16, 4 -> кратно
+
+System.Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(System.Console.ReadLine()); //конвертируем строку в число
+System.Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(System.Console.ReadLine()); //конвертируем строку в число
+
+if (number2 < number1 | number2 <= 0 | number1 <= 0)
+{
+    System.Console.WriteLine("Невозможно определить кратность второго числа первому");
+}
+else
+{
+    if (number2 % number1 == 0)
+    {
+        System.Console.WriteLine(number2 + " кратно " + number1);
+    }
+    else
+    {
+        System.Console.WriteLine(number2 + " делится на " + number1 + " с остатком " + number2 % number1);
+}
+}
---
+++ 02_lesson/02Task/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и 
+//проверяет, кратно ли оно одновременно 7 и 23. 
+//14 -> нет 46 -> нет 161 -> да
+
+System.Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(System.Console.ReadLine()); //конвертируем строку в число
+
+if (number1 < 23 * 7)
+{
+    System.Console.WriteLine("Невозможно определить кратность числа 23 и 7");
+}
+else
+{
+    if (number1 % (23 * 7) == 0)
+    {
+        System.Console.WriteLine(number1 + " одновременно кратно и 23, и 7");
+    }
+    else
+    {
+        System.Console.WriteLine(number1 + " не кратно одновременно и 23, и 7");
+}
+}
