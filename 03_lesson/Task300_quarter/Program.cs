5296a0959820d9f76544ad4d44dcdc13a5103657
Katerina Grishaeva <ithis@yandex.ru>
2022-08-07 21:43:04 +0300

    добавлены 2 решения задачи на определение квадранта координатной плоскости. Изучены свитч, интерполяция строк, написан встроенный тест (без учета подачи не числовых значений. Изучены математические термины на английском языке.

---
+++ 03_lesson/Task300_quarter/Program.cs
@@ -0,0 +1,58 @@
+﻿// Task300_quarter
+// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+
+void DisplayMeasurements(int a, int b) //оператором если и подачей координат априори без нуля
+{    
+        if (a > 0 & b > 0)
+            {
+                Console.WriteLine($"x-coordinate is {a}, y-coordinate is {b}. It is quadrant I"); //о, как! можно интерполировать строки
+            }
+        else if (a < 0 & b > 0)
+            {
+                Console.WriteLine($"x-coordinate is {a}, y-coordinate is {b}. It is quadrant II");
+            }   
+        else if  (a < 0 & b < 0)
+            {
+                Console.WriteLine($"x-coordinate is {a}, y-coordinate is {b}. It is quadrant III");
+            }
+        else if  (a > 0 & b < 0)
+            {
+                Console.WriteLine($"x-coordinate is {a}, y-coordinate is {b}. It is quadrant IV");
+            }             
+        else
+            {
+                Console.WriteLine("The point lies on the axis.");
+            }
+}
+
+//тесты
+Console.WriteLine("========Test=========");
+DisplayMeasurements(3, 4);  // Output: quadrant I
+DisplayMeasurements(-5, 5);  // Output: quadrant II
+DisplayMeasurements(-3, -4);  // Output: quadrant III
+DisplayMeasurements(3, -4);  // Output: quadrant IV
+DisplayMeasurements(0, -4);  // Output: The point lies on the axis
+DisplayMeasurements(0, 4);  // Output: The point lies on the axis
+DisplayMeasurements(5, 0);  // Output: The point lies on the axis
+DisplayMeasurements(-5, 0);  // Output: The point lies on the axis
+DisplayMeasurements(0, 0);  // Output: The point lies on the axis
+Console.WriteLine("========End Of Test=========");
+System.Console.WriteLine();
+
+int x=0; //начальные значения 0, чтобы цикл хотя бы раз сработал
+int y=0;
+while(x == 0 || y == 0) //задаем случайные координаты, не лежащие на осях
+{
+    //x = new Random().Next(-1, 1);
+    //y = new Random().Next(-1, 1);
+    x = new Random().Next(-1000000000, 1000000000);
+    y = new Random().Next(-1000000000, 1000000000);
+    System.Console.WriteLine("x = " + x + "y = " + y);
+}
+
+System.Console.WriteLine("x = " + x + "; y = " + y);
+DisplayMeasurements(x, y);  // Output: The point lies on the axis
+
---
+++ 03_lesson/Task300_quarter_case/Program.cs
@@ -0,0 +1,48 @@
+﻿// Task300_quarter with switch
+// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+void DisplayMeasurements(int a, int b)
+{
+    switch ((a, b)) //поиграем со свичом
+    {
+        case (> 0, > 0):
+            Console.WriteLine($"x-coordinate is {a}, y-coordinate is {b}. It is quadrant I"); //о, как! можно интерполировать строки
+            break;
+
+        case (< 0, > 0):
+            Console.WriteLine($"x-coordinate is {a}, y-coordinate is {b}. It is quadrant II"); 
+            break;    
+        case (< 0, < 0):
+            Console.WriteLine($"x-coordinate is {a}, y-coordinate is {b}. It is quadrant III"); 
+            break;
+
+        case (> 0, < 0):
+            Console.WriteLine($"x-coordinate is {a}, y-coordinate is {b}. It is quadrant IV"); 
+            break; 
+        default:
+            Console.WriteLine("The point lies on the axis.");
+            break;
+    }
+}
+
+//тесты
+Console.WriteLine("========Test=========");
+DisplayMeasurements(3, 4);  // Output: quadrant I
+DisplayMeasurements(-5, 5);  // Output: quadrant II
+DisplayMeasurements(-3, -4);  // Output: quadrant III
+DisplayMeasurements(3, -4);  // Output: quadrant IV
+DisplayMeasurements(0, -4);  // Output: The point lies on the axis
+DisplayMeasurements(0, 4);  // Output: The point lies on the axis
+DisplayMeasurements(5, 0);  // Output: The point lies on the axis
+DisplayMeasurements(-5, 0);  // Output: The point lies on the axis
+DisplayMeasurements(0, 0);  // Output: The point lies on the axis
+Console.WriteLine("========End Of Test=========");
+System.Console.WriteLine();
+
+//отправим случайные координаты без проверки на принадлежность оси
+int x = new Random().Next(-1000000000, 1000000000);
+int y = new Random().Next(-1000000000, 1000000000);
+System.Console.WriteLine("x = " + x + "; y = " + y);
+DisplayMeasurements(x, y);  // Output: The point lies on the axis
+
