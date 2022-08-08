c68a00ff69c6610030dd602a2fc520a9d3934629
Katerina Grishaeva <ithis@yandex.ru>
2022-08-08 22:05:27 +0300

    добавила задания с семинара №3

---
+++ 03_lesson/301Task_distance/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними на координатной плоскости
+
+double x1 = new Random().Next(-1000, 1000);
+double y1 = new Random().Next(-1000, 1000);
+System.Console.WriteLine("x1 = " + x1 + "; y1 = " + y1);
+
+double x2 = new Random().Next(-1000, 1000);
+double y2 = new Random().Next(-1000, 1000);
+System.Console.WriteLine("x2 = " + x2 + "; y2 = " + y2);
+
+double distance = Math.Sqrt ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+System.Console.WriteLine("Расстояние между заданными точками координатной плоскости равно " + distance);
+
---
+++ 03_lesson/302Task_square_of _number/Program.cs
@@ -0,0 +1,24 @@
+﻿// 302Task_square_of _number
+// Задача 22: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+System.Console.WriteLine(); //для красоты
+System.Console.WriteLine("============================="); //для красоты
+System.Console.WriteLine(); //для красоты
+
+int x = new Random().Next(2, 10);
+System.Console.WriteLine("x = " + x);
+System.Console.WriteLine();
+System.Console.WriteLine($"Квадраты натуральных чисел от 1 до {x}: "); //интерполяция строк с помощью значка доллара
+int i = 1;
+
+while (i <= x)
+{
+    System.Console.Write(i * i + "; ");
+    i++;
+}
+
+System.Console.WriteLine(); //для красоты
+System.Console.WriteLine(); //для красоты
+System.Console.WriteLine("============================="); //для красоты
+System.Console.WriteLine(); //для красоты
