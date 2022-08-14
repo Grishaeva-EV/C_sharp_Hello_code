751f41add377c757033c6a7f2710fb9e3027f6bd
Katerina Grishaeva <ithis@yandex.ru>
2022-08-14 22:14:42 +0300

    Task_602_Decimal_to_Binary_Conversion whithout DAMM 1, but only print

--- 06_lesson/Task_602_Decimal_to_Binary_Conversion/Program.cs
+++ 06_lesson/Task_602_Decimal_to_Binary_Conversion/Program.cs
@@ -1,14 +1,5 @@
 ﻿// Task_602_Decimal_to_Binary_Conversion
 
-// void PrintArrayFor(int[] col, int ind) //выводим на печать ind элементов массива
-// {
-//     for (int j = 0; j < ind; j++)
-//         {
-//             System.Console.Write(col[j] + "  ");
-//         }
-// }
-
-//int Decimal_to_Binary_Conversion(int x, int[] arr = new int[100000], int i = 0)
 int f(int x)
 {
     if (x == 0) 
@@ -16,20 +7,22 @@ int f(int x)
         System.Console.WriteLine("bottom");
         return 1;
     }
+    string str = string.Empty;
     System.Console.Write(x + "  ");
     // System.Console.Write((f(x/2) % 2)*10 + (f(x) % 2 + "  "));
     f(x/2);
     System.Console.Write(x % 2);
+    // str = str + Convert.ToString(x % 2);
+    // System.Console.WriteLine(Convert.ToString(x % 2));
 
-    return x % 2;
+    // str = str + "A";
+    // System.Console.WriteLine(str);
+    return 0;
 }
 
-int A = new Random().Next(100, 1000);
-
-// int[] arr = new int[100000];
-// int i = 0;
-
-
-int smth = f(A);
-System.Console.WriteLine(smth);
+//int smth = 
+f(291);
+//Convert.ToString(f(291))
+// System.Console.WriteLine();
+// System.Console.WriteLine(f(291));
 
