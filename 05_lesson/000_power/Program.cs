493497e58c6d9a85deeaaadff813e7082b3c9594
Katerina Grishaeva <ithis@yandex.ru>
2022-08-14 14:38:29 +0300

    // HomeWork_500_count_even: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве

---
+++ 05_lesson/000_power/Program.cs
@@ -0,0 +1,70 @@
+﻿// // HomeWork400_power_x RECURSION
+// In two positive integer numbers a and b
+// out a to the power b (a^b)
+// Write you own Power without using multiplication(*) and division(/) operators
+
+// The multiplication says how many times to use the number in summation 
+// The power of a number says how many times to use the number in a multiplication
+
+
+void WonderfulFrame() //для красоты
+{
+    System.Console.WriteLine(); 
+    System.Console.WriteLine("=============================");
+    System.Console.WriteLine(); 
+}
+
+int read() //проверяем, что там введено, ддосим, пока не получим натуральное число больше 1
+{      
+    bool result = int.TryParse(Console.ReadLine(), out int A);
+    //if (result != true || A < 2) //проверка типа введенного значения
+    while(result != true || A < 2) // убрала if, т.к. цикл выполнится только если условие тру
+    {
+        Console.WriteLine("Вы ввели не целое число больше 1. Повторите попытку.");
+        result = int.TryParse(Console.ReadLine(), out A);
+    }    
+    return(A);
+}
+
+int multiplication_x_y(int x, int y) 
+{
+    if (y < 1) {
+        System.Console.WriteLine("дошли до дна");
+        return 0;
+        
+        }
+    else
+        System.Console.WriteLine($"прямой ход x= {x}; y= {y};");
+    x = multiplication_x_y(x, y-1) + x;
+        System.Console.WriteLine($"j,hfn ход x= {x}; y= {y};");
+    return x;
+}
+
+//тело программы
+WonderfulFrame();
+
+Console.WriteLine("Введите целое число больше 1 ");
+int a = read();
+System.Console.WriteLine();
+
+Console.WriteLine("Введите целое число больше 1 ");
+int b = read();
+System.Console.WriteLine();
+
+// int power = b;
+
+multiplication_x_y(a, b);
+
+// for (int i = 1; i < b; i++)
+// {
+//     power = multiplication_x_y(a, a);
+//     Console.WriteLine($"{a} в степени {i + 1} = " + power);
+// }
+
+// Console.WriteLine($"{a} в степени {b} равно " + power);
+
+//     if (b < 2) {return a;}
+//     else {power = multiplication_x_y( multiplication_x_y(a, b-1), a);}
+
+
+WonderfulFrame();
---
+++ 05_lesson/HomeWork_500_count_even/Program.cs
@@ -0,0 +1,29 @@
+﻿// HomeWork_500_count_even
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] FillArray(int n)
+{
+    int[] arr = new int[n];
+    for (int ind = 0; ind < n; ind++)
+    {
+        arr[ind] = new Random().Next(100, 999);
+        System.Console.Write(arr[ind] + "  ");
+    }
+    return arr;
+}
+
+int N = new Random().Next(10, 20);
+System.Console.WriteLine($"Дан массив случайных трехзначных натуральных чисел длиной {N}");
+System.Console.WriteLine();
+
+int[] array = FillArray(N);
+System.Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < N; i++)
+{
+    if(array[i] % 2 == 0) {count++;}
+}
+
+System.Console.WriteLine($"Количество четных элементов массива равно {count}");
