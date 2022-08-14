cd0ffa0fae5542b7c597d4b2752287f78fd51702
Katerina Grishaeva <ithis@yandex.ru>
2022-08-14 18:28:23 +0300

    Task_601_valid_sides_for_triangle: Check whether triangle is valid or not if sides are given

---
+++ 06_lesson/Task_600_reverse_array/Program.cs
@@ -0,0 +1,44 @@
+﻿// Task_600_reverse_array
+// Напишите программу, которая перевернёт одномерный массив
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+
+
+int[] FillArray(int n) //целочисленный случайно заполненный массив с заданной длиной
+{
+    int[] arr = new int[n];
+    for (int ind = 0; ind < n; ind++)
+    {
+        arr[ind] = new Random().Next(1, 100000);
+        System.Console.Write(arr[ind] + "  "); //кажется, что вызов цикла для заполнения, а потом метода с циклом для печати
+        //будет дольше, чем печать сразу при создании элемента
+    }
+    return arr;
+}
+
+void PrintArrayFor(int[] col) //выводим на печать массив 
+{
+    for (int i = 0; i < col.Length; i++)
+        {
+            System.Console.Write(col[i] + "  ");
+        }
+}
+
+int N = new Random().Next(10, 20); //но длину тож случайно зададим, что уж тут
+System.Console.WriteLine($"Дан массив случайных чисел длиной {N}"); //ха-ха, дан... да...
+System.Console.WriteLine();
+
+int[] array = FillArray(N);
+System.Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < (N / 2); i++) //если идти дальше середины, то развернем снова и получится первоначальный порядок
+//если количество элементов нечетное, то центральный элемент не трогаем, т.к. N = 2* N/2 +1 из-за целочисленного деления Си шарп
+{
+    count = array[i]; //it's a classic: swapping two variable value with using third variable
+    array[i] = array[N - i - 1]; //и ошибка тож по классике, единичку из длины не вычла для нулевого индекса
+    array[N - i - 1] = count;
+}
+
+System.Console.WriteLine("Таки получите свой вывернутый массив, одномерный");
+PrintArrayFor(array);
---
+++ 06_lesson/Task_601_valid_sides_for_triangle/Program.cs
@@ -0,0 +1,23 @@
+﻿// Task_601_valid_sides_for_triangle
+// Check whether triangle is valid or not if sides are given
+// Везет же, когда пытаешься в английский, а тебе твою ж задачу сразу полным переводом на тематическом сайте
+
+int A = new Random().Next(1, 5); // для повышения вероятности равенства суммы двух чисел третьему, 
+int B = new Random().Next(1, 5); // уменьшим диапазон случайных значений
+int C = new Random().Next(1, 10);
+
+
+// int A = 2; // тесты
+// int B = 3; 
+// int C = 5;
+
+System.Console.WriteLine();
+System.Console.WriteLine("Даны числа:");
+System.Console.WriteLine("A = " + A);
+System.Console.WriteLine("B = " + B);
+System.Console.WriteLine("C = " + C);
+System.Console.WriteLine();
+
+if (A >= B + C || B >= C + A || C >= A + B) {System.Console.WriteLine("Треугольник невозможен");}
+else {System.Console.WriteLine("Условие существования треугольника выполняется");}
+System.Console.WriteLine();
